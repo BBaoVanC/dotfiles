69eb971369d5fcfa4321f22ec7743ae61791592e
BBaoVanC <github@bbaovanc.com>
2020-10-26 02:59:32 -0500

    Clone with depth 0 in zsh_plugins and remove dotfiles_setup.zsh

--- .zsh_plugins/clone.zsh
+++ .zsh_plugins/clone.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 echo "    Downloading zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo "    Done downloading zsh-syntax-highlighting"
