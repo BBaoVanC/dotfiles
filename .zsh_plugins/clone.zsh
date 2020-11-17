f1ac67a89d1c0878d4474d9ccd986da95c4c8d9c
BBaoVanC <github@bbaovanc.com>
2020-11-16 18:30:19 -0600

    Remove .zsh_plugins/

--- .zsh_plugins/clone.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-echo "    Downloading zsh-syntax-highlighting"
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
-echo "    Done downloading zsh-syntax-highlighting"
