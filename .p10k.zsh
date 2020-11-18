6a3786aa14020b8a4975fcbd0cdb2df001429e36
BBaoVanC <github@bbaovanc.com>
2020-11-18 00:10:05 -0600

    Move exit code to right before prompt character

--- .p10k.zsh
+++ .p10k.zsh
@@ -36,6 +36,7 @@
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
+    status                  # exit code of the last command
     prompt_char             # prompt symbol
   )
 
@@ -45,7 +46,6 @@
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
