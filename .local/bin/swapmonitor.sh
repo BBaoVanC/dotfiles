c5c2fbf6fae6f3ef27ae81be7df7267f2dfec9af
BBaoVanC <bbaovanc@bbaovanc.com>
2021-05-11 16:10:06 -0500

    Remove unused scripts

--- .local/bin/swapmonitor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while true; do
-    usage=$(free | grep Swap: | awk '{print $3}')
-    echo "$(date): using $usage bytes of swap" >> $XDG_CACHE_HOME/swapmonitor.txt
-    echo "Current swap usage is $usage bytes. Wrote this to $XDG_CACHE_HOME/swapmonitor.txt"
-    sleep 10
-done
