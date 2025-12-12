165f7a60cfbce1a445e264e04d390ab7e3055f96
BBaoVanC <bbaovanc@bbaovanc.com>
2025-12-12 17:34:13 -0600

    Change hyprpm installed plugin list to reinstall script
    Hyprland has broken one too many times and I am tired of manually
    reinstalling my plugins every month or two,

--- .local/share/hyprpm/INSTALLED.md
+++
@@ -1,4 +0,0 @@
-## Plugins used in this config
-
-- [hy3](https://github.com/outfoxxed/hy3)
-- [hyprsplit](https://github.com/shezdy/hyprsplit)
---
+++ .local/share/hyprpm/reinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+
+hyprpm update
+hyprpm add https://github.com/outfoxxed/hy3
+hyprpm enable hy3
+hyprpm add https://github.com/shezdy/hyprsplit
+hyprpm enable hyprsplit
